4df1dda8350bc4a938c144341c3b01fd0fbe8f47
hrithiksh101 <hrithik.sh.13@gmail.com>
2020-09-16 17:32:04 +0530

    Created Repo
    Made first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const queshions = [ 
+
+    { key : 1 , queshion : 'What is 2 + 2 ?' , options : [1,2,3,4] , answer : 4 } ,
+    { key : 2 , queshion : 'What is 2 + 3 ?' , options : [1,2,5,4] , answer : 5 } ,
+    { key : 3 , queshion : 'THe best city in the world according to you ?' , options : [ "New York" , "London" , "Paris" , "Sydney" ] , answer : 'New York' } ,
+    { key : 4 , queshion : 'What is 2 * 3 ?' , options : [1,6,5,4] , answer : 6 } ,
+    { key : 5 , queshion : 'What is 3/3 ?' , options : [1,2,5,4] , answer : 1 } ,
+
+
+ ] ;
+
+ // after framing all the queshions i will be displaying them on the screen
+
+
+ let QueshionNo = 1 ;
+
+
